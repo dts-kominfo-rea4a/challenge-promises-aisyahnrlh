5ec073d00c4f2a581f6fbc033a3448db40e5568c
Aisyah Nurul Hidayah <aisyahnurulh13@gmail.com>
2022-09-17 16:22:56 +0700

    solve promise challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const movieList1 = await promiseTheaterIXX();
+    const movieList2 = await promiseTheaterVGC();
+
+    const total1 = movieList1.filter((movie) => movie.hasil === emosi).length;
+    const total2 = movieList2.filter((movie) => movie.hasil === emosi).length;
+
+    return total1 + total2;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
